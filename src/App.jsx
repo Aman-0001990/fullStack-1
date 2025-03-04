5bc7ae7a0810b617b6c9f798363b44228c4fdc4f
AMAN AMAN <amanrohilla520@gmail.com>
2025-03-04 17:32:33 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css"; // Import the external CSS file
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1 className="heading">Personal Information</h1>
+        <p className="text"><strong>Name:</strong> Aman</p>
+        <p className="text"><strong>Email:</strong> amanrohilla520@gmail.com</p>
+        <p className="text"><strong>Phone:</strong> 785252451</p>
+        <p className="text"><strong>College:</strong> Chitkara University</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
